8143bc16cce5f72991d265299890a57fd8b21274
Patrick McCombs <pa.mccombs1@gmail.com>
2019-02-13 18:02:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,12 @@ class Student
   end
   
   def update
-    
+    sql = 
+    "UPDATE students 
+    SET name = ?, 
+    grade = ? 
+    WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id
   end
 
 
